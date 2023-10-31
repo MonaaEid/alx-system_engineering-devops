6a91b955936cc475fe89083a66b1efb8fe184e21
MonaaEid <monaa_eid@hotmail.com>
2023-10-31 22:39:28 +0200

    test

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 # Ruby script that accepts one argument and pass it to a regular expression matching method
 string = ARGV[0]
-regex = /^\\d{10}$/
+regex = /^\d{10}$/
 matches = string.scan(regex)
 puts matches.join
 
--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 # Ruby script that accepts one argument and pass it to a regular expression matching method
 string = ARGV[0]
-regex = /^[A-Z]+$/
+regex = /[A-Z]*/
 matches = string.scan(regex)
 puts matches.join
