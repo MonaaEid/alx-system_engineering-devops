2cc7f03a531a8bd27ea9e3a0b9e1d8e776792bd3
MonaaEid <monaa_eid@hotmail.com>
2023-10-31 22:24:15 +0200

    test

---
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+# Ruby script that accepts one argument and pass it to a regular expression matching method
+string = ARGV[0]
+regex = /hbt*n/
+matches = string.scan(regex)
+puts matches.join
+
