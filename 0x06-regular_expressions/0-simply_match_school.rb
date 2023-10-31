74557ad2894f382b8e3f5d2d296f3577d682fb55
MonaaEid <monaa_eid@hotmail.com>
2023-10-31 19:01:18 +0200

    test

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -2,8 +2,5 @@
 # Ruby script that accepts one argument and pass it to a regular expression matching method
 string = ARGV[0]
 regex = /School/
-if string.match(regex)
-  puts "Match found!"
-else
-  puts "No match found."
-end
+matches = string.scan(regex)
+puts matches.join
