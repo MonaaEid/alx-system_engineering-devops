6f48f3c370c693c3361185cdee50d30cabb7cda0
MonaaEid <monaa_eid@hotmail.com>
2023-12-22 06:45:04 -0800

    Add First task1

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# create a file in /tmp.
+file { '/tmp/school':
+  ensure  => 'file',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+  mode    => '0744'
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,5 @@
+Intro to Configuration Management
+Puppet resource type
+Puppet’s Declarative Language: Modeling Instead of Scripting
+Puppet lint
+Puppet emacs mode
