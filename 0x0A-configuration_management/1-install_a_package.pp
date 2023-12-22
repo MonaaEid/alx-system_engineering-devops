cd235a1725c30fdc478a5ea78c12193adc44ef6e
MonaaEid <monaa_eid@hotmail.com>
2023-12-22 09:06:19 -0800

    Add First task1/

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,5 @@
 # Using Puppet, install flask from pip3.
 package { 'flask':
   provider => 'pip3',
-  ensure  => '2.1.0' 
+  ensure   => '2.1.0' 
 }
-#mod 'pbailey-flask_bootstrap', '0.1.0'
-#exec {
-#py -3 -m venv venv
-#venv\Scripts\activate
-#pip install Flask -v 2.1.0
