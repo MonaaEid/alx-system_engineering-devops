1f15698901d7c917f8f3846c9e5093df637124e4
MonaaEid <monaa_eid@hotmail.com>
2024-01-04 02:41:08 +0200

    test

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,18 +1,56 @@
-# Install the nginx module
-puppet module install puppet-nginx
-
-# Create the index file
-file { '/var/www/html/index.html':
-  ensure  => file,
-  content => '<html><body><h1>Hello World!</h1></body></html>',
+exec { 'add nginx stable repo':
+  command => 'sudo add-apt-repository ppa:nginx/stable',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
-# Define the virtual host
-nginx::resource::server { 'example.com':
-  ensure        => present,
-  listen_port   => 80,
-  www_root      => '/var/www/html',
-  rewrite_rules => [
-    '^/redirect_me(.*)$ https://www.youtube.com permanent',
-  ],
+exec { 'update packages':
+  command => 'apt-get update',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
+package { 'nginx':
+  ensure     => 'installed',
+}
+
+file { '/var/www/html/index.html':
+  content => "Hello World!\n",
+}
+
+file { '/var/www/html/404.html':
+  content => "Ceci n'est pas une page\n",
+}
+
+
+file { 'Nginx default config file':
+  ensure  => file,
+  path    => '/etc/nginx/sites-enabled/default',
+  content =>
+"server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+               root /var/www/html;
+        # Add index.php to the list if you are using PHP
+        index index.html index.htm index.nginx-debian.html;
+        server_name _;
+        location / {
+                # First attempt to serve request as file, then
+                # as directory, then fall back to displaying a 404.
+                try_files \$uri \$uri/ =404;
+        }
+        error_page 404 /404.html;
+        location  /404.html {
+            internal;
+        }
+        
+        if (\$request_filename ~ redirect_me){
+            rewrite ^ https://www.youtube.com/ permanent;
+        }
+}
+",
+}
+
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
