6ef458ef84f75db34d601484066fb6fab4b79937
MonaaEid <monaa_eid@hotmail.com>
2024-01-04 02:46:23 +0200

    test

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -20,36 +20,6 @@ file { '/var/www/html/404.html':
   content => "Ceci n'est pas une page\n",
 }
 
-
-file { 'Nginx default config file':
-  ensure  => file,
-  path    => '/etc/nginx/sites-enabled/default',
-  content =>
-"server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-               root /var/www/html;
-        # Add index.php to the list if you are using PHP
-        index index.html index.htm index.nginx-debian.html;
-        server_name _;
-        location / {
-                # First attempt to serve request as file, then
-                # as directory, then fall back to displaying a 404.
-                try_files \$uri \$uri/ =404;
-        }
-        error_page 404 /404.html;
-        location  /404.html {
-            internal;
-        }
-        
-        if (\$request_filename ~ redirect_me){
-            rewrite ^ https://www.youtube.com/ permanent;
-        }
-}
-",
-}
-
-
 service { 'nginx':
   ensure  => running,
   require => Package['nginx'],
