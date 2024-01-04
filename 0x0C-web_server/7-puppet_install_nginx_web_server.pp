4e760277e7d864472323e94ab79f1e34be18c0e9
MonaaEid <monaa_eid@hotmail.com>
2024-01-04 03:27:44 +0200

    test

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,7 +1,7 @@
 #test
 
 package { 'nginx':
-  ensure     => 'installed',
+  ensure     => installed,
 }
 
 file { '/var/www/html/index.html':
@@ -11,11 +11,8 @@ file { '/var/www/html/index.html':
 file { '/var/www/html/404.html':
   content => "Ceci n'est pas une page\n",
 }
-exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/https://www.youtube.com\/;\\n\\t}/" /etc/nginx/sites-available/default':
-  provider => shell,
-}
 
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+exec {'run':
+  command  => 'sudo service nginx restart',
+  provider => shell,
 }
