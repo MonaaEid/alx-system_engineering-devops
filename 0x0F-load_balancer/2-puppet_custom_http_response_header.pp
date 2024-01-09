48c6a4b66ef18c373ee401b15c1d1bf162f4207b
MonaaEid <monaa_eid@hotmail.com>
2024-01-09 02:48:18 +0200

    update

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,8 +2,7 @@
 exec { 'command':
     command  => 'sudo apt-get -y update;
     sudo apt-get -y install nginx;
-    string="http {\n\tadd_header X-Served-By \"$HOSTNAME\";"
-    sudo sed -i "s/http {/$string/" /etc/nginx/nginx.conf;
+    sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/nginx.conf;
     sudo service nginx start',
 
 }
