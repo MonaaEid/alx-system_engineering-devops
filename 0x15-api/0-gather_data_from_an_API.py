486b8512becdb0e1c55fa77637abb360f14877c4
MonaaEid <monaa_eid@hotmail.com>
2024-01-30 00:47:51 +0200

    test

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """using this REST API, for a given employee ID, returns
-information about his/her TODO list progress."""
+information about his/her todo list progress."""
 import urllib.request
 import urllib.parse
 import urllib.error
