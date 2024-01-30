7f794fdf84d99e4290b3e15a29544b818b6e2ebe
MonaaEid <monaa_eid@hotmail.com>
2024-01-30 02:28:13 +0200

    test

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -11,10 +11,6 @@ if __name__ == "__main__":
     username = user.get("username")
     todo = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    completed_tasks = [{"task": t.get("title"), "completed": t.get(
-        "completed"), "username": user.get("name")} for t in todo]
-    data = {sys.argv[1]: completed_tasks}
-    filename = "{}.json".format(sys.argv[1])
     with open("{}.json".format(user), "w") as jsonfile:
         json.dump({user: [{
                 "task": t.get("title"),
