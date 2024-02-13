6c3570586887e47e637d0cf3715922957bc2d3b0
MonaaEid <monaa_eid@hotmail.com>
2024-02-13 22:01:15 +0200

    test

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
     """function that queries the Reddit API"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
-    request = urllib.request.Request(url, headers=headers)
+    request = urllib.request.Request(url, headers=headers, allow_redirects=False)
     with urllib.request.urlopen(request) as response:
         html = response.read()
         data = json.loads(html)
