a2ccbebfd393c6cce46b407ad2a455c66709dcb4
MonaaEid <monaa_eid@hotmail.com>
2024-02-15 00:22:37 +0200

    test

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,13 +8,12 @@ import json
 
 def number_of_subscribers(subreddit):
     """function that queries the Reddit API"""
+    if subreddit is None or type(subreddit) is not str:
+        return 0
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     request = urllib.request.Request(url, headers=headers)
     with urllib.request.urlopen(request) as respose:
         html = respose.read()
         data = json.loads(html)
-        if respose.status == 200:
-            return data.get('data').get('subscribers', 0)
-        else:
-            return 0
+        return data.get('data').get('subscribers', 0)
